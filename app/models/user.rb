afb67e02f89e730dbe626c515854e6c5e807e890
IvanciuVlad <ivanciu.vlad@yahoo.com>
2020-09-01 14:35:54 +0300

    Added friend search

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,31 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (username_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.username_matches(param)
+    matches('username', param)
+  end  
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject {|user| user.id == self.id}
+  end
+
+  def not_friends_with?(if_of_friend)
+    !self.friends.where(id: if_of_friend).exists?
+  end
 end
